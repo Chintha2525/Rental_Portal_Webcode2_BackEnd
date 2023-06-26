9fceb7bc3d4d19dc1a4f0b1c947e7a518060e2c1
Chintha2525 <dinesharun2525@gmail.com>
2023-06-26 14:04:14 +0530

    first commit

---
+++ schemas/userSchemas.js
@@ -0,0 +1,74 @@
+const validator = require("validator")
+const mongoose = require("mongoose")
+
+let userSchema = new mongoose.Schema(
+    {
+        name:{type:String,required:true},
+        email:{
+            type:String,
+            required:true,
+            lowercase:true,
+            validate:(value)=>{
+                return validator.isEmail(value)
+            }
+        },
+        password:{type:String,required:true},
+        role:{type:String,default:'user'},
+        createdAt:{type:Date,default:Date.now}
+    },
+    {
+       collection:'admin',
+       versionKey:false
+    }
+)
+
+
+let ContactSchema = new mongoose.Schema(
+    {
+        Name:{type:String,required:true},
+        Email:{
+            type:String,
+            required:true,
+            lowercase:true,
+            validate:(value)=>{
+                return validator.isEmail(value)
+            }
+        },
+        Message:{type:String,required:true},
+        Mobile:{type:String,required:false},
+        CreatedAt:{type:Date,default:Date.now},
+    },
+    
+    {
+       collection:'contacts',
+       versionKey:false
+    }
+)
+
+
+
+let ProductSchema = new mongoose.Schema(
+    {
+        Name:{type:String,required:true},
+        Price:{type:String,required:true},
+        Url:{type:String,required:true},
+        Quantity:{type:String,required:true},
+        Hours:{type:String,required:true},
+    },
+    
+    {
+       collection:'products',
+       versionKey:false
+    }
+)
+
+
+let AdminModel = mongoose.model('admin', userSchema)
+
+let ContactModel = mongoose.model("contact", ContactSchema)
+
+let ProductModel = mongoose.model("products", ProductSchema)
+
+module.exports={ AdminModel, ContactModel, ProductModel }
+
+
