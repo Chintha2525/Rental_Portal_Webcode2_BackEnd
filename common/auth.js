e5ac96c044f961a87777e9a60679af6f214165d7
Chintha2525 <dinesharun2525@gmail.com>
2023-06-28 12:48:41 +0530

    ini

--- common/auth.js
+++ common/auth.js
@@ -18,69 +18,116 @@ const createToken = async(payload)=>{
     return token
 }
 
+// const validate = (req, res, next) => {
+//     const token = req.headers.authorization.split(" ")[1];
+//     if (!token) {
+//       return res.status(401).send({ message: "Token Not Found" });
+//     }
+  
+//     try {
+//       const decoded = jwt.verify(token, process.env.SECRET_KEY);
+//       if (Math.floor(Date.now() / 1000) < decoded.exp) {
+//         next();
+//       } else {
+//         res.status(401).send({ message: "Token Expired" });
+//       }
+//     } catch (error) {
+//       res.status(500).send({ message: "Invalid Token", error });
+//     }
+//   };
+
+//   // const roleAdminGaurd = (req, res, next) => {
+//   //   // const token = req.headers.authorization.split(" ")[1];
+//   //   const authHeader = req.headers.authorization;
+//   // if (!authHeader) {
+//   //   return res.status(401).send({ message: "Authorization header not found" });
+//   // }
+
+//   // const token = authHeader.split(" ")[1];
+//   //   if (!token) {
+//   //     return res.status(401).send({ message: "Token Not Found" });
+//   //   }
+  
+//   //   try {
+//   //     const decoded = jwt.verify(token, process.env.SECRET_KEY);
+//   //     if (decoded.role === "admin") {
+//   //       next();
+//   //     } else {
+//   //       res.status(401).send({ message: "Only Admins are Allowed" });
+//   //     }
+//   //   } catch (error) {
+//   //     res.status(500).send({ message: "Invalid Token", error });
+//   //   }
+//   // };
+
+//   const roleAdminGaurd = (req, res, next) => {
+//     const authHeader = req.headers.authorization;
+  
+//     if (!authHeader || !authHeader.startsWith("Bearer ")) {
+//       return res.status(401).send({ message: "Token not found in Authorization header" });
+//     }
+  
+//     const token = authHeader.split(" ")[1];
+  
+//     try {
+//       const decoded = jwt.verify(token, process.env.SECRET_KEY);
+  
+//       if (decoded.role === "admin") {
+//         next();
+//       } else {
+//         res.status(401).send({ message: "Only admins are allowed" });
+//       }
+//     } catch (error) {
+//       res.status(500).send({ message: "Invalid token", error });
+//     }
+//   };
+
+
+
 const validate = (req, res, next) => {
-    const token = req.headers.authorization.split(" ")[1];
-    if (!token) {
-      return res.status(401).send({ message: "Token Not Found" });
-    }
-  
-    try {
-      const decoded = jwt.verify(token, process.env.SECRET_KEY);
-      if (Math.floor(Date.now() / 1000) < decoded.exp) {
-        next();
-      } else {
-        res.status(401).send({ message: "Token Expired" });
-      }
-    } catch (error) {
-      res.status(500).send({ message: "Invalid Token", error });
-    }
-  };
+  const authHeader = req.headers.authorization;
 
-  // const roleAdminGaurd = (req, res, next) => {
-  //   // const token = req.headers.authorization.split(" ")[1];
-  //   const authHeader = req.headers.authorization;
-  // if (!authHeader) {
-  //   return res.status(401).send({ message: "Authorization header not found" });
-  // }
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).send({ message: "Token not found in Authorization header" });
+  }
 
-  // const token = authHeader.split(" ")[1];
-  //   if (!token) {
-  //     return res.status(401).send({ message: "Token Not Found" });
-  //   }
-  
-  //   try {
-  //     const decoded = jwt.verify(token, process.env.SECRET_KEY);
-  //     if (decoded.role === "admin") {
-  //       next();
-  //     } else {
-  //       res.status(401).send({ message: "Only Admins are Allowed" });
-  //     }
-  //   } catch (error) {
-  //     res.status(500).send({ message: "Invalid Token", error });
-  //   }
-  // };
+  const token = authHeader.split(" ")[1];
 
-  const roleAdminGaurd = (req, res, next) => {
-    const authHeader = req.headers.authorization;
-  
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res.status(401).send({ message: "Token not found in Authorization header" });
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+
+    if (Math.floor(Date.now() / 1000) < decoded.exp) {
+      next();
+    } else {
+      res.status(401).send({ message: "Token Expired" });
     }
-  
-    const token = authHeader.split(" ")[1];
-  
-    try {
-      const decoded = jwt.verify(token, process.env.SECRET_KEY);
-  
-      if (decoded.role === "admin") {
-        next();
-      } else {
-        res.status(401).send({ message: "Only admins are allowed" });
-      }
-    } catch (error) {
-      res.status(500).send({ message: "Invalid token", error });
+  } catch (error) {
+    res.status(500).send({ message: "Invalid Token", error });
+  }
+};
+
+const roleAdminGaurd = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).send({ message: "Token not found in Authorization header" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+
+    if (decoded.role === "admin") {
+      next();
+    } else {
+      res.status(401).send({ message: "Only admins are allowed" });
     }
-  };
+  } catch (error) {
+    res.status(500).send({ message: "Invalid token", error });
+  }
+};
+
   
 
 module.exports = { hashPassword, hashCompare, createToken, validate, roleAdminGaurd }
